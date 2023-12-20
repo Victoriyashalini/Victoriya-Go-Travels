bbca459a05d73ea041f699501151317dd86cf601
Victoriyashalini <131896241+Victoriyashalini@users.noreply.github.com>
2023-12-20 21:33:34 +0530

    Delete nav.js

--- nav.js
+++
@@ -1,8 +0,0 @@
-const navlinkS= document.querySelector('.nav-link');
-const windowpathname= window.location.pathname;
-
-navlinkS.array.forEach(navlinkS=> {
-	if(navlinkS.href.includes(windowpathname)){
-		navlinkS.classlist.add('active');
-	}
-});
